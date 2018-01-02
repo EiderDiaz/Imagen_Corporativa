46e38b007408db6ec65dfc0f03dd65e65accdacd
EiderDiaz <eiderdiazmoraila@hotmail.com>
2018-01-02 00:31:59 -0700

    multi-idiomas ,multiple pantallas

---
+++ app/src/main/java/com/example/eider/imagen_corporativa/Idiomas_Multipantallas.java
@@ -0,0 +1,13 @@
+package com.example.eider.imagen_corporativa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Idiomas_Multipantallas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idiomas__multipantallas);
+    }
+}
--- app/src/main/java/com/example/eider/imagen_corporativa/MainActivity.java
+++ app/src/main/java/com/example/eider/imagen_corporativa/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.eider.imagen_corporativa;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Idiomas_Multipantallas.class);
+                startActivity(intent);
+            }
+        });
     }
 }
